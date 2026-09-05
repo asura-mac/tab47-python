a64395c8550527d96f1f606bea9f3adbe6d35b5b
Partha Sarma <127847695+asura-mac@users.noreply.github.com>
2026-09-05 19:48:50 +0530

    Add sicalculator.py to variables-and-data-types

---
+++ variables-and-data-types/sicalculator.py
@@ -0,0 +1,10 @@
+principal = 100
+rate = 5
+time = 2
+simple_interest = (principal * rate * time) / 100
+total_amount = principal + simple_interest
+print(principal)
+print(rate)
+print(time)
+print(simple_interest)
+print(total_amount)
